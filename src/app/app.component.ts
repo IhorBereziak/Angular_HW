462db11be1d68f16e9cb0b8d404ac3e691403907
Ihor <berezjak@i.ua>
2020-04-14 13:58:32 +0300

    add posts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {UserModel} from '../models/UserModel';
+import {UserService} from './services/user.service';
+import {PostModel} from '../models/PostModel';
+import {PostService} from './services/post.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<h1>hello {{msg}}!</h1>
+    <app-user *ngFor="let u of users" [user]="u"></app-user>
+    <app-post *ngFor="let p of posts" [post]="p"></app-post>
+  `,
+  styles: [`h1 {
+    background: silver
+  }`]
 })
 export class AppComponent {
-  title = 'prac1';
+  msg = 'users';
+  users: UserModel[];
+  posts: PostModel[];
+  constructor(private userService: UserService, private postService: PostService) {
+    this.userService.getUsers().subscribe(value => this.users = value);
+    this.postService.getPosts().subscribe(value => this.posts = value);
+  }
 }
